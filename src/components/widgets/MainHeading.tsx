d94d220bc1fec1426d62ebf309b143d960d42991
Zain <zainqureshi145@gmail.com>
2023-03-09 06:58:53 +0500

    removed useEffect -> unused

--- src/components/widgets/MainHeading.tsx
+++ src/components/widgets/MainHeading.tsx
@@ -1,32 +1,39 @@
 import { RoughNotation } from 'react-rough-notation';
 import { useStore } from '@nanostores/react';
 import { themeStore } from '~/nanostore/themeStore';
-import { useState } from 'react';
 
 const title = 'ignitedevs';
 let color = '#004aab';
 
-const MainHeading = () => {
-  // set the state
-  const [animationDuration, setAnimationDuration] = useState(5000);
-  // read the store value with 'useStore' hook
+const checkThemeColor = () => {
+  // using nanostore to get value of 'theme' from localstorage
   const $theme = useStore(themeStore);
-
   if ($theme === 'dark') {
     color = '#004aab';
-    //setAnimationDuration(animationDuration + 1);
   } else {
     color = 'pink';
-    //setAnimationDuration(animationDuration + 2);
   }
-
   console.log('Theme: ' + $theme, '-> Highlight Color: ' + color);
+};
+
+const MainHeading = () => {
+  checkThemeColor();
+  // const annotate = (annotation) => {
+  //   console.log('inside annotation callback...');
+  // };
 
   return (
     <div className="dark:text-white highlight p-10">
       <div>We are </div>
-      <div className="text-sky-800 p-5 dark:text-white">
-        <RoughNotation type="highlight" show color={color} animationDuration={5000} padding={20}>
+      <div className="myHeading text-sky-800 p-5 dark:text-white">
+        <RoughNotation
+          show={true}
+          type="highlight"
+          color={color}
+          animationDuration={7000}
+          padding={20}
+          //getAnnotationObject={annotate}
+        >
           {title}
         </RoughNotation>
       </div>
